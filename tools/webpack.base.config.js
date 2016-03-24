da539feab74bf95d25d6ee35acb27c39842b2cc8
Joshua Thornton <joshua@LT.local>
2016-03-24 14:03:36 +0800

    Building and app skeleton.

---
+++ tools/webpack.base.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+var path = require("path");
+
+module.exports = {
+  entry: './src/assets/js/main.js',
+  output: {
+    path: path.resolve(__dirname, '../public/assets/js'),
+    publicPath: '/assets/js/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      'window.jQuery': 'jquery'
+    }),
+    new webpack.ProvidePlugin({
+      Tether: "tether",
+      'window.Tether': 'tether'
+    }),
+    new webpack.ProvidePlugin({
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+
+  node: {
+    fs: "empty"
+  },
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react'),
+    },
+  }
+};
---
+++ tools/webpack.dev.config.js
@@ -0,0 +1,7 @@
+var webpackConfig = require('./webpack.base.config');
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = Object.assign({}, webpackConfig, {
+});
+
