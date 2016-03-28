46d15297f1316b265b5409526c8b4306ab1a78cf
Joshua Thornton <joshua@LT.local>
2016-03-28 13:41:52 +0800

    Account Summary outline.

---
+++ src/assets/js/components/layout/AccountSummary.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class AccountSummary extends Component {
+  render() {
+    return (
+      <div>
+        <span>Bob</span>
+      </div>
+    );
+  }
+};
+
+export default AccountSummary;
--- src/assets/js/components/layout/DrawNav.js
+++ src/assets/js/components/layout/DrawNav.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Menu from './Menu';
+import AccountSummary from './AccountSummary';
 
 class DrawNav extends Component {
   render() {
     return (
       <nav role="navigation">
+        <AccountSummary/>
         <Menu/>
       </nav>
     );
