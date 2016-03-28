8c0bf1f91e28f24f9bb7a919ebd409cd60717cc2
Joshua Thornton <joshua@LT.local>
2016-03-28 14:53:59 +0800

    Putting the sys alert in the screen.

--- src/assets/js/components/screens/Home.js
+++ src/assets/js/components/screens/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import DrawNav from '../layout/DrawNav';
 import DrawNavController from '../hoc/DrawNavController';
+import SysAlertManager from '../common/SysAlertManager';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <SysAlertManager/>
         <DrawNav
           isOpen={this.props.drawNav.get('isOpen')}
           onToggleMenuClick={this.props.onDrawNavToggleMenuClick}/>
