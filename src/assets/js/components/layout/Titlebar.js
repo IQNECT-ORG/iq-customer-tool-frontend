d648a09517ceb5fc0e365a3cc77f8716d6b318aa
Joshua Thornton <joshua@LT.local>
2016-03-29 11:33:13 +0800

    Titlebar now takes classnames.

--- src/assets/js/components/layout/Titlebar.js
+++ src/assets/js/components/layout/Titlebar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import classNames from 'classnames';
 
 class Titlebar extends Component {
   render() {
+    const className = classNames('titlebar clearfix', this.props.className);
     return (
-      <div className="titlebar">
+      <div className={className}>
+        {this.props.children}
       </div>
     );
   }
