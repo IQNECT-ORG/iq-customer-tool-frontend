806aec21940b1ec562163da3cb47fcfe3d953a26
Joshua Thornton <joshua@LT.local>
2016-03-29 10:26:21 +0800

    Some filler components.

---
+++ src/assets/js/components/common/forms/SearchForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}>
+      </form>
+    );
+  }
+};
+
+export default SearchForm;
---
+++ src/assets/js/components/layout/Titlebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Titlebar extends Component {
+  render() {
+    return (
+      <div className="titlebar">
+      </div>
+    );
+  }
+};
+
+export default Titlebar;
