00bbb173f7051c50acf9c7648e7256af7ff56fb4
Joshua Thornton <joshua@LT.local>
2016-03-30 16:27:52 +0800

    Routes to catalogues.

--- src/assets/js/routes/index.js
+++ src/assets/js/routes/index.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import App from '../components/App';
 import Home from '../components/screens/Home';
 import Auth from '../components/screens/Auth';
+import Catalogue from '../components/screens/Catalogue';
+import CatalogueBrands from '../components/screens/CatalogueBrands';
 import { Route, IndexRoute } from 'react-router';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home}/>
     <Route path="signin" component={Auth}/>
+    <Route path="catalogue" component={Catalogue}/>
+    <Route path="catalogue/brands" component={CatalogueBrands}/>
   </Route>
 );
