d94ba4a50dc0a8938e0531769185899d99a610a6
Joshua Thornton <joshua@LT.local>
2016-03-30 15:59:46 +0800

    Basic non-working add brand form.

---
+++ src/assets/js/components/common/forms/AddBrandForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AddBrandForm extends Component {
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}>
+        <fieldset className="form-group">
+          <label htmlFor="exampleInputEmail1" className="sr-only">Email address</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" placeholder="Enter email"/>
+        </fieldset>
+
+        <button type="submit" className="btn btn-primary">Add Brand</button>
+      </form>
+    );
+  }
+};
+
+export default AddBrandForm;
