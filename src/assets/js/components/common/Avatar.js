23183e20350909f78a9b87c9a2ad4440ac218240
Joshua Thornton <joshua@LT.local>
2016-03-30 12:03:55 +0800

    Allowing for image and icon based avatars.

--- src/assets/js/components/common/Avatar.js
+++ src/assets/js/components/common/Avatar.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 
 class Avatar extends Component {
   render() {
+    if(this.props.src) {
+      return this._renderImage();
+    } else if(this.props.icon) {
+      return this._renderIcon();
+    }
+  }
+
+  _renderImage() {
     return (
-      <div className="avatar">
+      <div className="avatar avatar--image">
         <div className="avatar__image-container">
           <img src={this.props.src}/>
+        </div>
+      </div>
+    );
+  }
+
+  _renderIcon() {
+    return (
+      <div className="avatar avatar--icon">
+        <div className="avatar__icon-container">
+          <span>Icon</span>
         </div>
       </div>
     );
