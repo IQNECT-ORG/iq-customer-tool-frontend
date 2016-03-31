2fca3352dd283b0e3edae26c473b2fe01c74fb77
Joshua Thornton <joshua@LT.local>
2016-03-31 13:55:09 +0800

    Basic outline of the create magazine form.

---
+++ src/assets/js/components/common/forms/CreateMagazineCampaignForm.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import joid from 'joid';
+
+class CreateMagazineCampaignForm extends Component {
+  render() {
+
+
+    return (
+      <div className="row">
+        <form className="form--content">
+          <div className="col-xs-12 col-sm-6">
+            <div className="pane">
+
+            </div>
+          </div>
+
+          <div className="col-xs-12 col-sm-6">
+            <div className="pane pane--filled">
+              <div className="pane__body">
+                <div className="row">
+                  <div className="col-xs-12">
+                    <h2>Basic Details</h2>
+
+                    <fieldset className="form-group">
+                      <label htmlFor={joid.link(true, 'input')}>Campaign Title</label>
+                      <input type="text" className="form-control" id={joid.link(false, 'input')} placeholder="Demo Name"/>
+                    </fieldset>
+
+                    <fieldset className="form-group">
+                      <label htmlFor={joid.link(true, 'input')}>Magazine Language</label>
+                      <input type="text" className="form-control" id={joid.link(false, 'input')} placeholder="English"/>
+                    </fieldset>
+
+                    <div className="row">
+                      <div className="col-xs-12">
+                        <label htmlFor={joid.link(true, 'input')}>Campaign Period</label>
+                      </div>
+                      <div className="col-xs-12 col-sm-6">
+                        <fieldset className="form-group">
+                          <div className="input-group">
+                            <input type="text" className="form-control" id={joid.link(false, 'input')} placeholder="From"/>
+                            <div className="input-group-addon">i</div>
+                          </div>
+                        </fieldset>
+                      </div>
+                      <div className="col-xs-12 col-sm-6">
+                        <fieldset className="form-group">
+                          <label className="sr-only" htmlFor={joid.link(true, 'input')}>From</label>
+                          <div className="input-group">
+                            <input type="text" className="form-control" id={joid.link(false, 'input')} placeholder="To"/>
+                            <div className="input-group-addon">i</div>
+                          </div>
+                        </fieldset>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor={joid.link(true, 'input')}>Default Target</label>
+                      <div className="row">
+                        <div className="col-xs-8 col-md-9 col-lg-10">
+                          <input type="text" className="form-control" id={joid.link(false, 'input')} placeholder="Amount"/>
+                        </div>
+                        <div className="col-xs-4 col-md-3 col-lg-2">
+                          <button type="button" className="btn btn-block btn-secondary-outline">i</button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="row">
+                      <div className="col-xs-6">
+                        <button type="button" className="btn btn-block btn-secondary">Back</button>
+                      </div>
+
+                      <div className="col-xs-6">
+                        <button type="submit" className="btn btn-block btn-primary">Next</button>
+                      </div>
+                    </div>
+
+                    <hr/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+};
+
+export default CreateMagazineCampaignForm;
---
+++ src/assets/js/components/screens/campaign/CreateCampaign.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import DrawNav from '../../layout/DrawNav';
+import DrawNavController from '../../hoc/DrawNavController';
+import SysAlertManager from '../../common/SysAlertManager';
+import AuthRequired from '../../hoc/AuthRequired';
+import Titlebar from '../../layout/Titlebar';
+import CreateMagazineCampaignForm from '../../common/forms/CreateMagazineCampaignForm';
+
+class CreateCampaign extends Component {
+  static get contextTypes() {
+    return {
+      store: React.PropTypes.object
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <DrawNav
+          isOpen={this.props.drawNav.get('isOpen')}
+          onToggleMenuClick={this.props.onDrawNavToggleMenuClick}/>
+
+        <main className="page-content" role="main">
+          <div className="container-fluid">
+            <SysAlertManager alerts={this.props.alerts}/>
+          </div>
+
+          <div className="container-fluid">
+            <div className="row">
+              <Titlebar className="col-xs-12">
+                <div className="row">
+                  <div className="col-xs-12">
+                    <h1>Create Campaign</h1>
+                  </div>
+                </div>
+              </Titlebar>
+            </div>
+          </div>
+
+          <div className="container">
+            <CreateMagazineCampaignForm/>
+          </div>
+        </main>
+      </div>
+    );
+  }
+};
+
+export default AuthRequired(DrawNavController(CreateCampaign));
