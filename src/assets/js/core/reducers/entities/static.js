52364614696f9ac71507435fdbc2c35de8c46b83
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-07 12:34:16 +0800

    Basic static reducer.

---
+++ src/assets/js/core/reducers/entities/static.js
@@ -0,0 +1,10 @@
+import Immutable from 'immutable';
+import Constants from '../Constants';
+import { combineReducers, createReducer } from 'redux-immutablejs';
+
+const Actions = Constants.ActionTypes;
+
+export default createReducer(new Immutable.Map({
+
+}), {
+});
