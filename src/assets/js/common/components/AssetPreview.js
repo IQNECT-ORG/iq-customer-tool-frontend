78be3612022943332c4cff39adc47ce35d39bdf8
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-07 13:55:01 +0800

    AssetPreviewer now supports non-image files.

--- src/assets/js/common/components/AssetPreview.js
+++ src/assets/js/common/components/AssetPreview.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 
 class AssetPreview extends Component {
   render() {
+    let core;
+    const imageTest = /^image\/(jpeg|png|gif|svg\+xml)$/;
+
+    if(imageTest.test(this.props.type) === true) {
+      core = this._renderImage();
+    } else {
+      core = this._renderIcon();
+    }
+
     return (
       <div className="asset-preview">
-        <img src={this.props.src}/>
+        {core}
       </div>
+    );
+  }
+
+  _renderImage() {
+    return (
+      <img src={this.props.src}/>
+    );
+  }
+
+  _renderIcon() {
+    return (
+      <div>Test</div>
     );
   }
 };
