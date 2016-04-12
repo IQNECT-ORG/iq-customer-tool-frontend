a7f02cf217d0fcc42211a0c62ea2ec1357bbdaac
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-12 15:49:36 +0800

    Now using layout segments to better render parts.

--- src/assets/js/common/components/layouts/Default.js
+++ src/assets/js/common/components/layouts/Default.js
@@ -4,21 +4,50 @@ import SystemAlertMessagesContainer from 'app/common/containers/SystemAlertMessa
 
 class DefaultLayout extends Component {
   render() {
+    const layoutRender = this.props.layoutRender || ::this.defaultLayoutRender;
+    const asideRender = this.props.asideRender || ::this.defaultAsideRender;
+    const titleRender = this.props.titleRender || ::this.defaultTitleRender;
+    const messageRender = this.props.messageRender || ::this.defaultMessageRender;
+    const contentRender = this.props.contentRender || ::this.defaultContentRender;
+
+    return layoutRender(asideRender, titleRender, messageRender, contentRender);
+  }
+
+  defaultLayoutRender(asideRender, titleRender, messageRender, contentRender) {
     return (
       <div>
-        <DrawNav/>
-
+        {asideRender()}
         <main className="page-content" role="main">
+          {titleRender()}
+
           <div className="container-fluid">
-            <SystemAlertMessagesContainer/>
+            {messageRender()}
           </div>
 
-          <div>
-            {this.props.children}
-          </div>
+          {contentRender()}
         </main>
       </div>
     );
+  }
+
+  defaultAsideRender() {
+    return (
+      <DrawNav/>
+    );
+  }
+
+  defaultTitleRender() {
+    return null;
+  }
+
+  defaultMessageRender() {
+    return (
+      <SystemAlertMessagesContainer/>
+    );
+  }
+
+  defaultContentRender() {
+    return this.props.children;
   }
 };
 
