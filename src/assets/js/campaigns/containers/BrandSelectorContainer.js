7202425bfb22f0ae504246bde20f934ea2b1b951
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-15 13:42:16 +0800

    Using correct id key.

--- src/assets/js/campaigns/containers/BrandSelectorContainer.js
+++ src/assets/js/campaigns/containers/BrandSelectorContainer.js
@@ -14,7 +14,7 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     onOptionClick: (e, brand) => {
-      dispatch(selectBrand(brand.id));
+      dispatch(selectBrand(brand.brandId));
     }
   };
 }
