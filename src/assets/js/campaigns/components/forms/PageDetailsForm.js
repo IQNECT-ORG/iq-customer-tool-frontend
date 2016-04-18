14b8245922f373c6690f831abdc86cd759f66a20
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-18 10:53:43 +0800

    Added last feature to detail page.

--- src/assets/js/campaigns/components/forms/PageDetailsForm.js
+++ src/assets/js/campaigns/components/forms/PageDetailsForm.js
@@ -127,7 +127,8 @@ class PageDetailForm extends Component {
             id={joid.link(false, 'input')}
             placeholder="Website"
             readOnly
-            {...this.props.fields.pages[this.props.pages].website}/>
+            {...this.props.fields.pages[this.props.pages].website}
+            onClick={this.props.onAddWebsiteClick}/>
           <span className="input-group-btn">
             <button className="btn btn-secondary" type="button" onClick={this.props.onWebsiteDeleteClick}>
               <i className="icons8-trash"/>
