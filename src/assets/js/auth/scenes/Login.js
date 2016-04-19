6af79a3ef96507b4ec15d203dbaff5c3716c9f1a
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-19 16:24:14 +0800

    Vertically align helper.
    Basic styling of the login.

--- src/assets/js/auth/scenes/Login.js
+++ src/assets/js/auth/scenes/Login.js
@@ -7,20 +7,28 @@ class Login extends Component {
   render() {
     return (
       <FocusLayout>
-        <h1>
-          Welcome to iQNECT
-          <br/>
-          Please Log in
-        </h1>
+        <div className="vertically-align">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-8 col-xs-offset-2">
+                <h1 className="type-colour-02 text-xs-center">
+                  Welcome to iQNECT
+                  <br/>
+                  Please Log in
+                </h1>
 
-        <div className="pane pane--filled">
-          <LoginFormContainer/>
+                <div className="pane pane--filled">
+                  <LoginFormContainer/>
+                </div>
+
+                <div className="text-xs-center">
+                  <span className="type-colour-02">Forgot your password? </span>
+                  <Link to="/forgotten-password" className="type-colour-02">Click here to reset</Link>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <span>
-          <span>Forgot your password?</span>
-          <Link to="/forgotten-password">Click here to reset</Link>
-        </span>
       </FocusLayout>
     );
   }
