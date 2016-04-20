617484c77384c93cf0a34ace85d7d507ef869d29
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-20 10:09:12 +0800

    Campaign catalogue now has a grid structure.

--- src/assets/js/common/components/campaignList/CampaignList.js
+++ src/assets/js/common/components/campaignList/CampaignList.js
@@ -6,19 +6,21 @@ import CampaignListFilterFormContainer from '../../containers/CampaignListFilter
 class CampaignList extends Component {
   render() {
     return (
-      <div className="campaign-list">
-        <CampaignListFilterFormContainer onFormSubmit={this.props.onFilterSubmit}/>
+      <div className="campaign-list m-t-1">
+        <div className="pane pane--filled p-a-1">
+          <CampaignListFilterFormContainer onFormSubmit={this.props.onFilterSubmit}/>
 
-        <div>
-          <span>Thumbnail</span>
-          <span>Type</span>
-          <span>Title</span>
-          <span>Period</span>
-          <span>Status</span>
+          <div className="row">
+            <div className="col-xs-2">Thumbnail</div>
+            <div className="col-xs-2">Type</div>
+            <div className="col-xs-2">Title</div>
+            <div className="col-xs-2">Period</div>
+            <div className="col-xs-2">Status</div>
+          </div>
+          <ul className="list-unstyled">
+            {this._renderRows()}
+          </ul>
         </div>
-        <ul className="list-unstyled">
-          {this._renderRows()}
-        </ul>
       </div>
     );
   }
--- src/assets/js/common/components/campaignList/CampaignListRow.js
+++ src/assets/js/common/components/campaignList/CampaignListRow.js
@@ -5,22 +5,22 @@ class CampaignListRow extends Component {
   render() {
     const { campaign } = this.props;
     return (
-      <li>
-        <span>
+      <li className="row">
+        <div className="col-xs-2">
           <CampaignListThumbnail/>
-        </span>
-        <span>{campaign.type}</span>
-        <span>{campaign.name}</span>
-        <span>NI - NI</span>
-        <span>NI</span>
-        <span>
+        </div>
+        <div className="col-xs-2">{campaign.type}</div>
+        <div className="col-xs-2">{campaign.name}</div>
+        <div className="col-xs-2">NI - NI</div>
+        <div className="col-xs-2">NI</div>
+        <div className="col-xs-2">
           <button
             type="button"
             className="btn btn-secondary"
             onClick={this.props.onDeleteClick}>
             Delete
           </button>
-        </span>
+        </div>
       </li>
     );
   }
