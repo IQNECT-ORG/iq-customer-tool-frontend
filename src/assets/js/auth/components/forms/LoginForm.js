f17c46e973d2359bb498d361a01a0290b91a857a
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-21 15:49:38 +0800

    Login placeholders.

--- src/assets/js/auth/components/forms/LoginForm.js
+++ src/assets/js/auth/components/forms/LoginForm.js
@@ -4,15 +4,15 @@ class LoginForm extends Component {
   render() {
     const fields = this.props.fields;
     return (
-      <form onSubmit={this.props.onSubmit}>
+      <form className="form--content" onSubmit={this.props.onSubmit}>
         <fieldset className="form-group">
           <label htmlFor="email">Email Address</label>
-          <input type="email" className="form-control" id="email" placeholder="Example input"
+          <input type="email" className="form-control" id="email" placeholder="name@example.com"
             {...fields.email}/>
         </fieldset>
         <fieldset className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" id="password" placeholder="Another input"
+          <input type="password" className="form-control" id="password" placeholder="password"
             {...fields.password}/>
         </fieldset>
 
