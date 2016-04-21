a980c38546f0b527e98e424434bbf52bbf7d8af6
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-21 10:23:46 +0800

    Importing required actions.

--- src/assets/js/core/sagas/campaign.js
+++ src/assets/js/core/sagas/campaign.js
@@ -1,6 +1,7 @@
 import { takeEvery, takeLatest } from 'redux-saga';
 import { call, put, take, fork, select } from 'redux-saga/effects';
 import brandActions from 'app/common/actions/brands';
+import * as routerActions from 'react-router-redux/lib/actions';
 
 function* watchLoadCampaignCreatePage() {
   yield takeEvery('LOAD_CAMPAIGN_CREATE_PAGE', function* () {
