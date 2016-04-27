774640f219ece1d3484749243f9ed244f08fb12d
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-27 14:05:30 +0800

    Basic success path of the image campaign.

--- test/specs/createCampaign.js
+++ test/specs/createCampaign.js
@@ -8,6 +8,7 @@ describe('Create Campaign', function() {
       browser.element('[href="/campaigns/create"]').click();
       browser.waitForExist('.selector__option', 20000);
       browser.element('.selector__option > button').click();
+
       // @TODO: This selector is poor
       browser.waitForExist('.selector__option .icons8-questions', 20000);
       browser.element('.selector__option .icons8-questions').click();
@@ -21,4 +22,23 @@ describe('Create Campaign', function() {
     });
   });
 
+  describe('Image', function() {
+    it('should create campaign', function() {
+      loginHelper.login();
+      browser.element('[href="/campaigns/create"]').click();
+      browser.waitForExist('.selector__option', 20000);
+      browser.element('.selector__option > button').click();
+
+      // @TODO: This selector is poor
+      browser.waitForExist('.selector__option .icons8-dashboard', 20000);
+      browser.element('.selector__option .icons8-dashboard').click();
+
+      browser.waitForExist('.asset-input__input', 20000);
+      browser.chooseFile('.asset-input__input', path.resolve(__dirname, '../assets/mI7qPUg.jpg'));
+      browser.element('[name="campaignTitle"]').setValue('Test campaign');
+      browser.element('[type="submit"]').click();
+
+      browser.waitForExist('.ReactModal__Overlay', 20000);
+    });
+  });
 });
