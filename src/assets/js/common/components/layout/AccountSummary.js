1251f0c939061c1588db3255712a434c7904df8d
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-27 16:33:36 +0800

    Account summary styles.

--- src/assets/js/common/components/layout/AccountSummary.js
+++ src/assets/js/common/components/layout/AccountSummary.js
@@ -5,7 +5,7 @@ import UserAvatar from '../UserAvatar';
 class AccountSummary extends Component {
   render() {
     return (
-      <div>
+      <div className="account-summary">
         <UserAvatar src="http://www.thetimes.co.uk/tto/multimedia/archive/00968/ad0f2a3e-50df-11e5-_968539c.jpg"/>
         <span className="user-name">Bob</span>
       </div>
