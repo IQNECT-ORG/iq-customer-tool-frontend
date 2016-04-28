c5ea9fa99914199dcb9f07090f475be451e71be1
Joshua Thornton <joshua.thornton@iqnect.org>
2016-04-28 18:00:13 +0800

    Better spacing on the campaign list.

--- src/assets/js/common/components/campaignList/CampaignList.js
+++ src/assets/js/common/components/campaignList/CampaignList.js
@@ -7,23 +7,25 @@ class CampaignList extends Component {
   render() {
     return (
       <div className="campaign-list m-t-1">
-        <div className="pane pane--filled p-a-1">
-          <CampaignListFilterFormContainer
-            placeholder="Search Campaigns"
-            onFormSubmit={this.props.onFilterSubmit}/>
+        <div className="pane pane--filled">
+          <div className="pane__body">
+            <CampaignListFilterFormContainer
+              placeholder="Search Campaigns"
+              onFormSubmit={this.props.onFilterSubmit}/>
 
-          <div className="faux-table campaign-list__table">
-            <div className="faux-table__head">
-              <div className="faux-table__cell col-xs-2">Thumbnail</div>
-              <div className="faux-table__cell col-xs-2">Type</div>
-              <div className="faux-table__cell col-xs-2">Title</div>
-              <div className="faux-table__cell col-xs-2">Period</div>
-              <div className="faux-table__cell col-xs-2">Status</div>
-              <div className="faux-table__cell col-xs-2">&nbsp;</div>
+            <div className="faux-table campaign-list__table">
+              <div className="faux-table__head">
+                <div className="faux-table__cell col-xs-2">Thumbnail</div>
+                <div className="faux-table__cell col-xs-2">Type</div>
+                <div className="faux-table__cell col-xs-2">Title</div>
+                <div className="faux-table__cell col-xs-2">Period</div>
+                <div className="faux-table__cell col-xs-2">Status</div>
+                <div className="faux-table__cell col-xs-2">&nbsp;</div>
+              </div>
+              <ul className="faux-table__body list-unstyled">
+                {this._renderRows()}
+              </ul>
             </div>
-            <ul className="faux-table__body list-unstyled">
-              {this._renderRows()}
-            </ul>
           </div>
         </div>
       </div>
