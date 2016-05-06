0c3a4e13f238c04abff8bfeebd5091e53df1f6d8
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-06 09:36:25 +0800

    Moving the success dialog as it is more of a common component.

--- src/assets/js/core/components/App.js
+++ src/assets/js/core/components/App.js
@@ -9,7 +9,7 @@ import AddCouponModal from 'app/common/modals/AddCoupon';
 import CreateCouponModal from 'app/common/modals/CreateCoupon';
 import PreviewWebsiteModal from 'app/common/modals/PreviewWebsite';
 import PreviewCouponModal from 'app/common/modals/PreviewCoupon';
-import SuccessModal from 'app/campaigns/modals/Success';
+import SuccessModal from 'app/common/modals/Success';
 import AddBrandModal from 'app/common/modals/AddBrand';
 import EditBrandModal from 'app/common/modals/EditBrand';
 import EditCouponModal from 'app/common/modals/EditCoupon';
