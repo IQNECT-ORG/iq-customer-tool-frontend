8d02945d3bdf770e87ccf3ada4a71dbcd4e2989d
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-11 14:45:42 +0800

    Calculating overalldata, genderdata, and location data.

--- src/assets/js/analytics/scenes/Overview.js
+++ src/assets/js/analytics/scenes/Overview.js
@@ -13,10 +13,6 @@ import rd3 from 'rd3';
 import _ from 'lodash';
 import moment from 'moment';
 
-const rawData = {"allSearches":[{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280567,"long":103.845847,"timestamp":1461833342.7299,"relTime":"2016-04-28 16:49:02.000000","age":33,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHXHCCu2gVRLjsIQAQsl6_exzit8zOKCCSl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHXHCCu2gVRLjsIQAQsl6_exzit8zOKCCSQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280597,"long":103.845862,"timestamp":1461833530.1187,"relTime":"2016-04-28 16:52:09.000000","age":33,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHVw0d3wkz9F2nHwPJd2ZIKC16MFkOJR7Il_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHVw0d3wkz9F2nHwPJd2ZIKC16MFkOJR7IQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280597,"long":103.845862,"timestamp":1461833580.7908,"relTime":"2016-04-28 16:53:00.000000","age":33,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHXHCCu2gVRLjIcenTAh4FqdHS-Yfz4Cz9l_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHXHCCu2gVRLjIcenTAh4FqdHS-Yfz4Cz9QGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280595,"long":103.845861,"timestamp":1461834616.4011,"relTime":"2016-04-28 17:10:16.000000","age":33,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHOExl-I0CHLhoVoxrTil8qDvNAEUIQq-Nl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHOExl-I0CHLhoVoxrTil8qDvNAEUIQq-NQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280594,"long":103.845878,"timestamp":1461834705.0105,"relTime":"2016-04-28 17:11:44.000000","age":33,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHXHCCu2gVRLgp5KBRAinRtlZIsznUUfwSl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHXHCCu2gVRLgp5KBRAinRtlZIsznUUfwSQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280585,"long":103.845868,"timestamp":1461834742.9858,"relTime":"2016-04-28 17:12:22.000000","age":33,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHXHCCu2gVRLg3gAXQjMKP3C6G_M-idkBOl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUHXHCCu2gVRLg3gAXQjMKP3C6G_M-idkBOQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280594,"long":103.845878,"timestamp":1461834802.616,"relTime":"2016-04-28 17:13:22.000000","age":33,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUH_N11TQ3hevEwzcA1noQaNCg4EV52tnsAl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn7VdbO6b6hUH_N11TQ3hevEwzcA1noQaNCg4EV52tnsAQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280587,"long":103.845848,"timestamp":1461895039.9959,"relTime":"2016-04-29 09:57:19.000000","age":0,"gender":"","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJV_N11TQ3hevF8NnbYN-Q8wU258Ni1qLhnl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJV_N11TQ3hevF8NnbYN-Q8wU258Ni1qLhnQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":0,"long":0,"timestamp":1461895351.2601,"relTime":"2016-04-29 10:02:31.000000","age":65,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJVVw0d3wkz9F2rh-kNG0jjm9T3WxJnsdeVl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJVVw0d3wkz9F2rh-kNG0jjm9T3WxJnsdeVQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280598,"long":103.845884,"timestamp":1461898245.6251,"relTime":"2016-04-29 10:50:45.000000","age":33,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJVOExl-I0CHLj4X0QvDlAbD2TKfBm5ANuwl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJVOExl-I0CHLj4X0QvDlAbD2TKfBm5ANuwQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.280598,"long":103.845884,"timestamp":1461898246.5417,"relTime":"2016-04-29 10:50:46.000000","age":33,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJV_N11TQ3hevG5xp4LZ2roGEDUbUVq2U41l_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJV_N11TQ3hevG5xp4LZ2roGEDUbUVq2U41QGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":1.2806,"long":103.845859,"timestamp":1461898377.5171,"relTime":"2016-04-29 10:52:57.000000","age":65,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJVOExl-I0CHLiXL8PU9mYrjm1mYZhrbnNZl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-Usn8YJqfJVOExl-I0CHLiXL8PU9mYrjm1mYZhrbnNZQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":0,"long":0,"timestamp":1462348696.116,"relTime":"2016-05-04 07:58:16.000000","age":16,"gender":"m","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIjOExl-I0CHLhRsImcwToTIHCVe-bhXctNl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIjOExl-I0CHLhRsImcwToTIHCVe-bhXctNQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":0,"long":0,"timestamp":1462348874.3101,"relTime":"2016-05-04 08:01:14.000000","age":0,"gender":"","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIjXHCCu2gVRLgUrn_Z-DezDbhp8Vy2Jjqrl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIjXHCCu2gVRLgUrn_Z-DezDbhp8Vy2JjqrQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":0,"long":0,"timestamp":1462350111.1159,"relTime":"2016-05-04 08:21:51.000000","age":0,"gender":"f","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIjOExl-I0CHLik3IRKkEzkg-I0aDkIcPZXl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIjOExl-I0CHLik3IRKkEzkg-I0aDkIcPZXQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":0,"long":0,"timestamp":1462350284.3328,"relTime":"2016-05-04 08:24:44.000000","age":26,"gender":"f","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIj_N11TQ3hevEmUsI3FpLu9IiyaJxGn93Hl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIj_N11TQ3hevEmUsI3FpLu9IiyaJxGn93HQGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":0,"long":0,"timestamp":1462350362.9008,"relTime":"2016-05-04 08:26:02.000000","age":26,"gender":"","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIjVw0d3wkz9F0GehmxLm4ZzCCgInLSe-c5l_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIjVw0d3wkz9F0GehmxLm4ZzCCgInLSe-c5QGGKINJOky9TkYoSJVPuwQ=="},{"brandId":12,"campaignId":84,"triggerId":105,"lat":0,"long":0,"timestamp":1462350364.3064,"relTime":"2016-05-04 08:26:04.000000","age":26,"gender":"","keyword":null,"imgPreview":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIj_N11TQ3hevEF78NDzTau2Ph0_KSKiBlsl_lcgtAP1CDiq4LUAkL0vg==","imgFullsize":"\/\/dev-static.iqnect.org\/PGtA0O6CD9SGovcw-QeKtCZPC8AoH0oaZfD_1DDQDlS5x_-eORXJn-k5Q3EQpMIj_N11TQ3hevEF78NDzTau2Ph0_KSKiBlsQGGKINJOky9TkYoSJVPuwQ=="}]}
-
-console.log(rawData);
-
 const mapStyle = [
   {
     'featureType': 'all',
@@ -47,23 +43,6 @@ const mapStyle = [
   }
 ];
 
-var lineData = [
-  {
-    name: 'series1',
-    values: [ { x: 0, y: 20 }, { x: 1, y: 30 }, { x: 2, y: 10 }, { x: 3, y: 5 }, { x: 4, y: 8 }, { x: 5, y: 15 }, { x: 6, y: 10 } ]
-  },
-  // {
-  //   name: 'series2',
-  //   values : [ { x: 0, y: 8 }, { x: 1, y: 5 }, { x: 2, y: 20 }, { x: 3, y: 12 }, { x: 4, y: 4 }, { x: 5, y: 6 }, { x: 6, y: 2 } ]
-  // },
-  // {
-  //   name: 'series3',
-  //   values: [ { x: 0, y: 0 }, { x: 1, y: 5 }, { x: 2, y: 8 }, { x: 3, y: 2 }, { x: 4, y: 6 }, { x: 5, y: 4 }, { x: 6, y: 2 } ]
-  // }
-];
-
-var pieData = [{label: "Margarita", value: 20.0}, {label: "John", value: 55.0}, {label: "Tim", value: 25.0 }];
-
 var barData = [
   {
     "name": "Series A",
@@ -83,29 +62,29 @@ class Overview extends Component {
   }
 
   render() {
-    const markers = [
-      {
-        position: {
-          lat: 0,
-          lng: 0,
-        },
-        key: `Taiwan`,
-        defaultAnimation: 2,
-        icon: markerIcon,
-        labelContent: "$425K",
-        //labelAnchor: new google.maps.Point(22, 0),
-        labelClass: "labels", // the CSS class for the label,
-        labelAnchor: new google.maps.Point(20, 20)
-      },
-      {
-        position: {
-          lat: 0,
-          lng: 0,
-        },
-        key: `Taiwan2`,
-        defaultAnimation: 2
-      }
-    ];
+    // const markers = [
+    //   {
+    //     position: {
+    //       lat: 0,
+    //       lng: 0,
+    //     },
+    //     key: `Taiwan`,
+    //     defaultAnimation: 2,
+    //     icon: markerIcon,
+    //     labelContent: "$425K",
+    //     //labelAnchor: new google.maps.Point(22, 0),
+    //     labelClass: "labels", // the CSS class for the label,
+    //     labelAnchor: new google.maps.Point(20, 20)
+    //   },
+    //   {
+    //     position: {
+    //       lat: 0,
+    //       lng: 0,
+    //     },
+    //     key: `Taiwan2`,
+    //     defaultAnimation: 2
+    //   }
+    // ];
 
     const timespan = {};
     _.times(12, n => {
@@ -116,7 +95,14 @@ class Overview extends Component {
       };
     });
 
-    const wow = _(rawData.allSearches)
+    var lineData = [
+      {
+        name: 'series1',
+        values: [ { x: 0, y: 20 }, { x: 1, y: 30 }, { x: 2, y: 10 }, { x: 3, y: 5 }, { x: 4, y: 8 }, { x: 5, y: 15 }, { x: 6, y: 10 } ]
+      }
+    ];
+
+    const overallData = _(this.props.data.allSearches)
       .sortBy('timestamp')
       .thru(value => {
         return _.reduce(value, (result, search) => {
@@ -140,9 +126,56 @@ class Overview extends Component {
       .thru(_.toArray)
       .value();
 
-    lineData[0].values = wow;
+    lineData[0].values = overallData;
 
-    console.log(wow);
+    const genderData = _(this.props.data.allSearches)
+      // Count each gender type
+      .thru(value => _.countBy(value, search => search.gender))
+      // Calculate the total of all genders
+      .thru(value => _.reduce(value, (result, count) => {
+        result.total += count;
+        return result;
+      }, {
+        total: 0,
+        data: value
+      }))
+      // Work out the percentage
+      .thru(value => _.reduce(value.data, (result, count, key) => {
+        result[key] = (count / value.total) * 100;
+        return result;
+      }, value.data))
+      // Converting the data to the chart format
+      .thru(value => _.reduce(value, (result, percentage, key) => {
+        result.push({
+          label: key,
+          value: percentage
+        });
+        return result;
+      }, []))
+      .value();
+
+    console.log(this.props.data.allSearches);
+
+    const markers = _(this.props.data.allSearches)
+      .thru(value => _.reduce(value, (result, search) => {
+        const key = `${search.lat}:${search.long}`;
+        result[key] = {
+          position: {
+            lat: search.lat,
+            lng: search.long,
+          },
+          key: key,
+          defaultAnimation: 2,
+          icon: markerIcon,
+          labelContent: '1',
+          //labelAnchor: new google.maps.Point(22, 0),
+          labelClass: 'labels', // the CSS class for the label,
+          labelAnchor: new google.maps.Point(20, 20)
+        };
+        return result;
+      }, {}))
+      .thru(_.toArray)
+      .value();
 
     return (
       <DefaultLayout
@@ -213,7 +246,7 @@ class Overview extends Component {
                         {props => {
                           return (
                             <rd3.PieChart
-                              data={pieData}
+                              data={genderData}
                               width={props.autoWidth}
                               height={props.autoWidth}
                               radius={props.autoWidth / 2}
@@ -308,6 +341,35 @@ class Overview extends Component {
                   </AutoWidth>
                 </div>
               </div>
+            </div>
+          </div>
+
+          <div className="pane pane--filled">
+            <div className="pane__body">
+              <GoogleMapLoader
+                containerElement={
+                  <div
+                    {...this.props}
+                    style={{
+                      height: 500,
+                    }}
+                  />
+                }
+                googleMapElement={
+                  <GoogleMap
+                    defaultZoom={3}
+                    defaultCenter={{lat: 0, lng: 0}}
+                    options={{
+                      styles: mapStyle
+                    }}
+                    onClick={_ => {}}>
+                    {markers.map((marker, index) => {
+                      return (
+                        <MarkerWithLabel {...marker}/>
+                      );
+                    })}
+                  </GoogleMap>
+                }/>
             </div>
           </div>
         </div>
