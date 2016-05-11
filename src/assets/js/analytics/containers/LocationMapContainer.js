b12c6bdec21e1d06797e938db045f3cfc240ea35
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-11 17:53:17 +0800

    Basic clustering of markers.
    Not fit for consumption.

--- src/assets/js/analytics/containers/LocationMapContainer.js
+++ src/assets/js/analytics/containers/LocationMapContainer.js
@@ -4,6 +4,8 @@ import _ from 'lodash';
 import moment from 'moment';
 import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
 import MarkerWithLabel from 'app/common/components/maps/MarkerWithLabel';
+import ui from 'redux-ui/transpiled';
+import geolib from 'geolib';
 
 const mapStyle = [
   {
@@ -46,39 +48,91 @@ const markerIcon = {
   anchor: new google.maps.Point(100,100)
 };
 
-const render = (props) => {
-  return (
-    <GoogleMapLoader
-      containerElement={
-        <div style={{
-          height: 500,
-        }}/>
-      }
-      googleMapElement={
-        <GoogleMap
-          defaultZoom={3}
-          defaultCenter={{lat: 0, lng: 0}}
-          options={{
-            styles: mapStyle
-          }}
-          onClick={_ => {}}>
-          {props.markers.map((marker, index) => {
-            return (
-              <MarkerWithLabel {...marker}/>
-            );
-          })}
-        </GoogleMap>
-      }/>
-  );
+const eventWrapper = (fn) => {
+  return function() {
+    const args = Array.prototype.slice.call(arguments);
+    args.unshift(this.refs.map);
+    fn.apply(null, args);
+  }
+};
+
+class LocationMap extends Component {
+  render() {
+    return (
+      <GoogleMapLoader
+        containerElement={
+          <div style={{
+            height: 500,
+          }}/>
+        }
+        googleMapElement={
+          <GoogleMap
+            ref="map"
+            defaultZoom={this.props.ui.zoom}
+            defaultCenter={{lat: 0, lng: 0}}
+            options={{
+              styles: mapStyle
+            }}
+            onClick={_ => {}}
+            onZoomChanged={eventWrapper(this.props.onZoomChanged).bind(this)}>
+            {this.props.markers.map((marker, index) => {
+              return (
+                <MarkerWithLabel {...marker}/>
+              );
+            })}
+          </GoogleMap>
+        }/>
+    );
+  }
 }
 
+
 const mapStateToProps = (state, ownProps) => {
+  console.log(ownProps.ui.zoom);
   const filters = state.analytics.filters;
   const data = state.analytics.data;
   const allSearches = data.allSearches;
 
   const markers = _(allSearches)
     .thru(value => _.reduce(value, (result, search) => {
+      const pool = _.find(result, poolItem => {
+        console.log(geolib.getDistance(
+          {
+            latitude: search.lat,
+            longitude: search.long
+          },
+          {
+            latitude: poolItem.lat,
+            longitude: poolItem.long
+          }
+        ));
+        return geolib.isPointInCircle(
+          {
+            latitude: search.lat,
+            longitude: search.long
+          },
+          {
+            latitude: poolItem.lat,
+            longitude: poolItem.long
+          },
+          10000 / (ownProps.ui.zoom + 1)
+        );
+      });
+
+      if(pool == null) {
+        result.push({
+          lat: search.lat,
+          long: search.long,
+          count: 1
+        });
+      } else {
+        pool.count++;
+      }
+
+      return result;
+    }, []))
+    .thru(value => _.reduce(value, (result, search) => {
+      console.log(search.lat, search.long);
       const key = `${search.lat}:${search.long}`;
       result[key] = {
         position: {
@@ -88,7 +142,7 @@ const mapStateToProps = (state, ownProps) => {
         key: key,
         defaultAnimation: 2,
         icon: markerIcon,
-        labelContent: '1',
+        labelContent: search.count,
         //labelAnchor: new google.maps.Point(22, 0),
         labelClass: 'labels', // the CSS class for the label,
         labelAnchor: new google.maps.Point(20, 20)
@@ -105,10 +159,21 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
+    onZoomChanged: (mapComponent) => {
+      const map = mapComponent.props.map;
+
+      ownProps.updateUI('zoom', map.zoom);
+    }
   };
 };
 
-let DecoratedComponent = render;
+let DecoratedComponent = LocationMap;
 DecoratedComponent = connect(mapStateToProps, mapDispatchToProps)(DecoratedComponent);
+DecoratedComponent = ui({
+  state: {
+    zoom: 3
+  }
+})(DecoratedComponent);
+
 
 export default DecoratedComponent;
