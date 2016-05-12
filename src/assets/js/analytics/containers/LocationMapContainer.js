8e867aa3053d2948e36acf9f346bded4589433d8
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-12 10:22:26 +0800

    Clustering of the map markers.

--- src/assets/js/analytics/containers/LocationMapContainer.js
+++ src/assets/js/analytics/containers/LocationMapContainer.js
@@ -4,8 +4,8 @@ import _ from 'lodash';
 import moment from 'moment';
 import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
 import MarkerWithLabel from 'app/common/components/maps/MarkerWithLabel';
+import MarkerClusterer from 'react-google-maps/lib/addons/MarkerClusterer';
 import ui from 'redux-ui/transpiled';
-import geolib from 'geolib';
 
 const mapStyle = [
   {
@@ -42,10 +42,16 @@ const markerIcon = {
   path: path,
   fillColor: '#e91e63',
   fillOpacity: 1,
-  scale: 1,
+  scale: 0.25,
   // strokeColor: '#e91e63',
   strokeWeight: 0,
   anchor: new google.maps.Point(100,100)
+};
+
+const clusterStyle = {
+  width: 50,
+  height: 50,
+  url: 'http://www.thetimes.co.uk/tto/multimedia/archive/00968/ad0f2a3e-50df-11e5-_968539c.jpg'
 };
 
 const eventWrapper = (fn) => {
@@ -75,11 +81,13 @@ class LocationMap extends Component {
             }}
             onClick={_ => {}}
             onZoomChanged={eventWrapper(this.props.onZoomChanged).bind(this)}>
-            {this.props.markers.map((marker, index) => {
-              return (
-                <MarkerWithLabel {...marker}/>
-              );
-            })}
+            <MarkerClusterer styles={[clusterStyle]}>
+              {this.props.markers.map((marker, index) => {
+                return (
+                  <MarkerWithLabel {...marker}/>
+                );
+              })}
+            </MarkerClusterer>
           </GoogleMap>
         }/>
     );
@@ -88,51 +96,12 @@ class LocationMap extends Component {
 
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(ownProps.ui.zoom);
   const filters = state.analytics.filters;
   const data = state.analytics.data;
   const allSearches = data.allSearches;
 
   const markers = _(allSearches)
     .thru(value => _.reduce(value, (result, search) => {
-      const pool = _.find(result, poolItem => {
-        console.log(geolib.getDistance(
-          {
-            latitude: search.lat,
-            longitude: search.long
-          },
-          {
-            latitude: poolItem.lat,
-            longitude: poolItem.long
-          }
-        ));
-        return geolib.isPointInCircle(
-          {
-            latitude: search.lat,
-            longitude: search.long
-          },
-          {
-            latitude: poolItem.lat,
-            longitude: poolItem.long
-          },
-          10000 / (ownProps.ui.zoom + 1)
-        );
-      });
-
-      if(pool == null) {
-        result.push({
-          lat: search.lat,
-          long: search.long,
-          count: 1
-        });
-      } else {
-        pool.count++;
-      }
-
-      return result;
-    }, []))
-    .thru(value => _.reduce(value, (result, search) => {
-      console.log(search.lat, search.long);
       const key = `${search.lat}:${search.long}`;
       result[key] = {
         position: {
@@ -142,7 +111,7 @@ const mapStateToProps = (state, ownProps) => {
         key: key,
         defaultAnimation: 2,
         icon: markerIcon,
-        labelContent: search.count,
+        labelContent: 1,
         //labelAnchor: new google.maps.Point(22, 0),
         labelClass: 'labels', // the CSS class for the label,
         labelAnchor: new google.maps.Point(20, 20)
