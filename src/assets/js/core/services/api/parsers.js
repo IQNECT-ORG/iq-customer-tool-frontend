ce38276e92f1443956eda309611ca0c8902b4d61
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-20 17:01:51 +0800

    Trying to ensure some form of unique primary key, but allows it to be recalculated.

--- src/assets/js/core/services/api/parsers.js
+++ src/assets/js/core/services/api/parsers.js
@@ -7,7 +7,7 @@ const triggerParser = (schema, data, options) => {
 
     item.payload = _.transform(payload, (result, payload, index) => {
       result.push({
-        triggerPayloadId: _.uniqueId(),
+        triggerPayloadId: `${item.triggerId}-${index}`,
         triggerId: item.triggerId,
         index,
         data: payload
@@ -27,7 +27,7 @@ const triggerParser = (schema, data, options) => {
 
 const trainingResultsParser = (schema, data, options) => {
   _.each(data, (item, index) => {
-    item.trainingResultId = _.uniqueId();
+    item.trainingResultId = `${options.triggerId}-${index}`;
     item.triggerId = options.triggerId;
     item.frame = index;
   });
