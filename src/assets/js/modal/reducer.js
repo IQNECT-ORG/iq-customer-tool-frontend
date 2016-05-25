2570b0d48f6db9f108692f228a0529332790040c
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-25 11:30:17 +0800

    Removing immutable from modals.

--- src/assets/js/modal/reducer.js
+++ src/assets/js/modal/reducer.js
@@ -1,20 +1,28 @@
-import Immutable from 'immutable';
 import Constants from './Constants';
-import { combineReducers, createReducer } from 'redux-immutablejs';
+import { createReducer } from 'redux-create-reducer';
 import undoable from 'redux-undo';
+import _ from 'lodash';
 
 const Actions = Constants.ActionTypes;
-const initialState = new Immutable.Map({
+const initialState = {
   isOpen: false,
   path: null,
-  data: new Immutable.Map()
-});
+  data: {}
+};
 
 let reducer = createReducer(initialState, {
-  [Actions.MODAL_CLOSE]: (state, action) => state.set('isOpen', false),
-  [Actions.MODAL_OPEN]: (state, action) => state.set('isOpen', true),
-  [Actions.MODAL_UPDATE_PATH]: (state, action) => state.set('path', action.payload.path),
-  [Actions.MODAL_UPDATE_DATA]: (state, action) => state.set('data', action.payload)
+  [Actions.MODAL_CLOSE]: (state, action) => _.assign({}, state, {
+    isOpen: false
+  }),
+  [Actions.MODAL_OPEN]: (state, action) => _.assign({}, state, {
+    isOpen: true
+  }),
+  [Actions.MODAL_UPDATE_PATH]: (state, action) => _.assign({}, state, {
+    path: action.payload.path
+  }),
+  [Actions.MODAL_UPDATE_DATA]: (state, action) => _.assign({}, state, {
+    data: action.payload
+  })
 });
 
 reducer = undoable(reducer, {
