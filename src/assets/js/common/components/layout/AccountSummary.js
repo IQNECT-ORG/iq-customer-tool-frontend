09a10bc4bd1ef8b4ed4659095165548a299068c8
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-25 11:38:03 +0800

    Temporary user fallback until one is provided.

--- src/assets/js/common/components/layout/AccountSummary.js
+++ src/assets/js/common/components/layout/AccountSummary.js
@@ -4,7 +4,7 @@ import UserAvatar from '../UserAvatar';
 export default (props) => {
   return (
     <div className="account-summary">
-      <UserAvatar src="http://www.thetimes.co.uk/tto/multimedia/archive/00968/ad0f2a3e-50df-11e5-_968539c.jpg"/>
+      <UserAvatar src="/assets/images/user-fallback.svg"/>
       <span className="user-name">
         {props.user.firstName} {props.user.lastName}
       </span>
