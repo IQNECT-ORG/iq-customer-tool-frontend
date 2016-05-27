38fed7154f6cf409dce0afa111ba3438bb924e66
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-27 13:24:37 +0800

    Minification options.

--- tools/webpack.prd.config.js
+++ tools/webpack.prd.config.js
@@ -6,7 +6,31 @@ var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const plugins = webpackConfig.plugins.slice();
 plugins.push(
   new LodashModuleReplacementPlugin,
-  new webpack.optimize.UglifyJsPlugin({minimize: true}),
+  new webpack.optimize.UglifyJsPlugin({
+    screw_ie8: true,
+    source_map: false,
+    compress: {
+      sequences: true,
+      properties: true,
+      dead_code: true,
+      drop_debugger: true,
+      conditionals: true,
+      comparisons: true,
+      evaluate: true,
+      booleans: true,
+      loops: true,
+      unused: true,
+      hoist_funs: true,
+      if_return: true,
+      join_vars: true,
+      cascade: true,
+      collapse_vars: true,
+      warnings: false,
+      drop_console: true,
+      keep_fargs: false,
+      keep_fnames: false,
+    }
+  }),
   new webpack.optimize.DedupePlugin(),
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': '"production"'
