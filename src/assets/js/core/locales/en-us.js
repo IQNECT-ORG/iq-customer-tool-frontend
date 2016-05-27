7fab173539aac45584c82ce60f6e477a8cb4dba6
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-27 11:32:05 +0800

    Formatting the i18n messages as a hash, then converting to dot notation.
    Adding a tonne more messages.

--- src/assets/js/core/locales/en-us.js
+++ src/assets/js/core/locales/en-us.js
@@ -1,7 +1,60 @@
+import dotize from 'dotize';
+
+export const rawMessages = {
+  app: {
+    addNewBrand: 'Add New brand',
+    addNewCoupon: 'Add New Coupon',
+    createImageCampaign: 'Create Image Campaign',
+    createMagazineCampaign: 'Create Magazine Campaign',
+    createVideoCampaign: 'Create Video Campaign',
+    next: 'Next',
+    back: 'Back',
+    save: 'Save',
+    del: 'Delete',
+    upload: 'Upload',
+    searchCampaigns: 'Search Campaigns',
+
+    campaignTypes: {
+      image: 'Image',
+      magazine: 'Magazine',
+      video: 'Video'
+    },
+
+    menu: {
+      createCampaign: 'Create Campaign',
+      dashboard: 'Dashboard',
+      catalogue: 'Manage',
+      analytics: 'Analytics',
+      help: 'Help',
+      brands: 'Brands',
+      campaigns: 'Campaigns',
+      coupons: 'Coupons'
+    },
+    analytics: {
+      header: 'Analytics',
+      headerSelectCampaign: 'Analytics / Select Campaign'
+    },
+    campaigns: {
+      headerSelectBrand: 'Select a Brand',
+      headerChooseCampaignType: 'Choose Campaign Type',
+      headerCreateCampaign: 'Create Campaign',
+      campaignDetails: 'Campaign Details',
+    },
+    catalogue: {
+      brands: {
+        header: 'Manage / Brands'
+      },
+      campaigns: {
+        header: 'Manage / Campaigns'
+      },
+      coupons: {
+        header: 'Manage / Coupons'
+      }
+    }
+  }
+}
+
 export default {
   locales: 'en-US',
-  messages: {
-    'app.analytics.header': 'Analytics',
-    'app.analytics.headerSelectCampaign': 'Analytics / Select Campaign'
-  }
+  messages: dotize.convert(rawMessages)
 };
