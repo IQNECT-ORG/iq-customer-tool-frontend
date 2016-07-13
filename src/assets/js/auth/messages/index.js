d817853cdab3bbc1065f8385f95ee5c77d44bd4a
Joshua Thornton <jshthornton@gmail.com>
2016-07-13 09:31:17 +0100

    Auth: Moved over to signal and message struct.

---
+++ src/assets/js/auth/messages/index.js
@@ -0,0 +1,28 @@
+import { createAction } from 'redux-actions';
+
+// Login
+export const M_AUTH_LOGIN_REQUEST = 'M_AUTH_LOGIN_REQUEST';
+export const M_AUTH_LOGIN_SUCCESS = 'M_AUTH_LOGIN_SUCCESS';
+export const M_AUTH_LOGIN_FAILURE = 'M_AUTH_LOGIN_FAILURE';
+
+export const authLoginRequest = createAction(M_AUTH_LOGIN_REQUEST);
+export const authLoginSuccess = createAction(M_AUTH_LOGIN_SUCCESS);
+export const authLoginFailure = createAction(M_AUTH_LOGIN_FAILURE);
+
+// Forgotten Password
+export const M_AUTH_FORGOTTEN_PASSWORD_REQUEST = 'M_AUTH_FORGOTTEN_PASSWORD_REQUEST';
+export const M_AUTH_FORGOTTEN_PASSWORD_SUCCESS = 'M_AUTH_FORGOTTEN_PASSWORD_SUCCESS';
+export const M_AUTH_FORGOTTEN_PASSWORD_FAILURE = 'M_AUTH_FORGOTTEN_PASSWORD_FAILURE';
+
+export const authForgottenPasswordRequest = createAction(M_AUTH_FORGOTTEN_PASSWORD_REQUEST);
+export const authForgottenPasswordSuccess = createAction(M_AUTH_FORGOTTEN_PASSWORD_SUCCESS);
+export const authForgottenPasswordFailure = createAction(M_AUTH_FORGOTTEN_PASSWORD_FAILURE);
+
+// Reset Password
+export const M_AUTH_RESET_PASSWORD_REQUEST = 'M_AUTH_RESET_PASSWORD_REQUEST';
+export const M_AUTH_RESET_PASSWORD_SUCCESS = 'M_AUTH_RESET_PASSWORD_SUCCESS';
+export const M_AUTH_RESET_PASSWORD_FAILURE = 'M_AUTH_RESET_PASSWORD_FAILURE';
+
+export const authResetPasswordRequest = createAction(M_AUTH_RESET_PASSWORD_REQUEST);
+export const authResetPasswordSuccess = createAction(M_AUTH_RESET_PASSWORD_SUCCESS);
+export const authResetPasswordFailure = createAction(M_AUTH_RESET_PASSWORD_FAILURE);
--- src/assets/js/auth/reducers/index.js
+++ src/assets/js/auth/reducers/index.js
@@ -1,26 +1,33 @@
-import Constants from '../Constants';
 import recycleState from 'redux-recycle';
-import { combineReducers } from 'redux';
 import { createReducer } from 'redux-create-reducer';
 import _ from 'lodash';
+import {
+  M_AUTH_LOGIN_REQUEST,
+  M_AUTH_LOGIN_SUCCESS,
+  M_AUTH_LOGIN_FAILURE,
+  M_AUTH_FORGOTTEN_PASSWORD_REQUEST,
+  M_AUTH_FORGOTTEN_PASSWORD_SUCCESS,
+  M_AUTH_FORGOTTEN_PASSWORD_FAILURE,
+  M_AUTH_RESET_PASSWORD_REQUEST,
+  M_AUTH_RESET_PASSWORD_SUCCESS,
+  M_AUTH_RESET_PASSWORD_FAILURE
+} from '../messages';
 
-const Actions = Constants.ActionTypes;
-
-const initialState = {
+export const initialState = {
   userId: null
 };
 
 let reducer = createReducer(initialState, {
-  [Actions.AUTH_LOGIN_SUCCESS]: (state, action) => {
+  [M_AUTH_LOGIN_SUCCESS]: (state, action) => {
     return _.assign({}, state, {
       userId: action.payload.result
     });
-  },
-  ['AUTH_AUTHENTICATE_SUCCESS']: (state, action) => {
-    return _.assign({}, state, {
-      userId: action.payload.result
-    });
-  }
+  }//,
+  // ['AUTH_AUTHENTICATE_SUCCESS']: (state, action) => {
+  //   return _.assign({}, state, {
+  //     userId: action.payload.result
+  //   });
+  // }
 });
 
 reducer = recycleState(reducer, [], initialState);
