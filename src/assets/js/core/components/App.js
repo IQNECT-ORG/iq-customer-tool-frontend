c51201641e187d01e2ea423e8700add11b72f507
Joshua Thornton <jshthornton@gmail.com>
2016-07-15 17:26:07 +0100

    Updating modal paths.

--- src/assets/js/core/components/App.js
+++ src/assets/js/core/components/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import ModalManager from 'app/modal/containers/ModalManager';
+import { ModalPaths } from 'app/common/Constants';
 // Modals
-import AddWebsiteModal from 'app/campaigns/modals/AddWebsite';
-import AddCouponModal from 'app/common/modals/AddCoupon';
-import CreateCouponModal from 'app/common/modals/CreateCoupon';
-import PreviewWebsiteModal from 'app/common/modals/PreviewWebsite';
-import PreviewCouponModal from 'app/common/modals/PreviewCoupon';
-import SuccessModal from 'app/common/modals/Success';
-import AddBrandModal from 'app/common/modals/AddBrand';
-import EditBrandModal from 'app/common/modals/EditBrand';
-import EditCouponModal from 'app/common/modals/EditCoupon';
-import CampaignCouponBrowserModal from 'app/campaigns/modals/CouponBrowser';
+import AddWebsiteModal from 'app/common/containers/modals/AddWebsite';
+import AddCouponModal from 'app/common/containers/modals/AddCoupon';
+import CreateCouponModal from 'app/common/containers/modals/CreateCoupon';
+import PreviewWebsiteModal from 'app/common/containers/modals/PreviewWebsite';
+import PreviewCouponModal from 'app/common/containers/modals/PreviewCoupon';
+import SuccessModal from 'app/common/containers/modals/Success';
+import AddBrandModal from 'app/common/containers/modals/AddBrand';
+import EditBrandModal from 'app/common/containers/modals/EditBrand';
+import EditCouponModal from 'app/common/containers/modals/EditCoupon';
+import CouponBrowserModal from 'app/common/containers/modals/CouponBrowser';
 
 class App extends Component {
   render() {
@@ -19,16 +20,20 @@ class App extends Component {
       <div className="app">
         {this.props.children}
         <ModalManager paths={{
-          addWebsite: AddWebsiteModal,
-          addCoupon: AddCouponModal,
-          previewWebsite: PreviewWebsiteModal,
-          createCoupon: CreateCouponModal,
-          previewCoupon: PreviewCouponModal,
-          success: SuccessModal,
-          addBrand: AddBrandModal,
-          editBrand: EditBrandModal,
-          editCoupon: EditCouponModal,
-          campaignCouponBrowser: CampaignCouponBrowserModal
+          // Website
+          [ModalPaths.WEBSITE_ADD]: AddWebsiteModal,
+          [ModalPaths.WEBSITE_PREVIEW]: PreviewWebsiteModal,
+          // Coupon
+          [ModalPaths.COUPON_ADD]: AddCouponModal,
+          [ModalPaths.COUPON_CREATE]: CreateCouponModal,
+          [ModalPaths.COUPON_PREVIEW]: PreviewCouponModal,
+          [ModalPaths.COUPON_EDIT]: EditCouponModal,
+          [ModalPaths.COUPON_BROWSER]: CouponBrowserModal,
+          // Brand
+          [ModalPaths.BRAND_CREATE]: AddBrandModal,
+          [ModalPaths.BRAND_EDIT]: EditBrandModal
+          // Misc
+          [ModalPaths.SUCCESS]: SuccessModal,
         }}/>
       </div>
     );
