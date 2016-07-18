ff3449fc63799f119b1af42ad74f82a095fc800a
Joshua Thornton <jshthornton@gmail.com>
2016-07-18 13:17:22 +0100

    Fixed missing var declaration.

--- src/assets/js/catalogue/containers/pages/IndexContainer.js
+++ src/assets/js/catalogue/containers/pages/IndexContainer.js
@@ -1,7 +1,7 @@
 import IndexPage from '../../components/pages/Index';
 import AuthenticationRequiredContainer from 'app/common/containers/hoc/AuthenticationRequiredContainer';
 
-DecoratedComponent = IndexPage;
+let DecoratedComponent = IndexPage;
 DecoratedComponent = AuthenticationRequiredContainer()(DecoratedComponent);
 
 export default DecoratedComponent;
