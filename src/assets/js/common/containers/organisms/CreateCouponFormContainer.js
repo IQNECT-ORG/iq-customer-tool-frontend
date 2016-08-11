6e635f20237e4e28293bfb51fce03a7443d7e302
Joshua Thornton <jshthornton@gmail.com>
2016-08-11 17:45:38 +0100

    Create coupon form now sends the action when submitting.

--- src/assets/js/common/containers/organisms/CreateCouponFormContainer.js
+++ src/assets/js/common/containers/organisms/CreateCouponFormContainer.js
@@ -2,23 +2,29 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import CouponFormContainer from './CouponFormContainer';
 import _ from 'lodash';
+import { couponCreateFormSubmit } from '../../signals';
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    initialValues: {
-    }
-  };
-};
+const mapStateToProps = void 0;
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch) => {
   return {
+    actions: bindActionCreators({
+      couponCreateFormSubmit
+    }, dispatch)
   };
 };
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => {
   return _.assign({}, stateProps, dispatchProps, ownProps, {
     onSubmit: values => {
-
+      return new Promise((resolve, reject) => {
+        dispatchProps.actions.couponCreateFormSubmit({
+          values,
+          reject,
+          resolve,
+          isModal: true
+        });
+      });
     }
   });
 };
