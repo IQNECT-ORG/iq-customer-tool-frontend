07503e59e0da1905930175ef066c45690cf4b4ec
Joshua Thornton <jshthornton@gmail.com>
2016-09-05 10:32:11 +0100

    Removed redundant fields from video campaign.

--- src/assets/js/campaigns/components/organisms/VideoCampaignForm.js
+++ src/assets/js/campaigns/components/organisms/VideoCampaignForm.js
@@ -36,37 +36,6 @@ class CreateCampaign extends Component {
                       <input type="text" className="form-control" id={joid.link(false, 'input')} placeholder="Demo Name" {...fields.name}/>
                     </fieldset>
 
-                    <div className="row">
-                      <div className="col-xs-12">
-                        <label htmlFor={joid.link(true, 'input')}>Campaign Period</label>
-                      </div>
-                      <div className="col-xs-12 col-sm-6">
-                        <fieldset className="form-group">
-                          <div className="input-group">
-                            <DateTimeField
-                              mode="date"
-                              id={joid.link(false, 'input')}
-                              placeholder="From"
-                              {...fields.campaignPeriodFrom}
-                              buttonIcon="icons8-date-from"/>
-                          </div>
-                        </fieldset>
-                      </div>
-                      <div className="col-xs-12 col-sm-6">
-                        <fieldset className="form-group">
-                          <label className="sr-only" htmlFor={joid.link(true, 'input')}>From</label>
-                          <div className="input-group">
-                            <DateTimeField
-                              mode="date"
-                              id={joid.link(false, 'input')}
-                              placeholder="To"
-                              {...fields.campaignPeriodTo}
-                              buttonIcon="icons8-date-to"/>
-                          </div>
-                        </fieldset>
-                      </div>
-                    </div>
-
                     <TargetType
                       values={{
                         url: this.props.values.url,
@@ -79,10 +48,6 @@ class CreateCampaign extends Component {
                       onAddWebsiteClick={this.props.onAddWebsiteClick}
                       onAddCouponClick={this.props.onAddCouponClick}
                       onWebsiteDeleteClick={this.props.onWebsiteDeleteClick}/>
-
-                    <TagsInput
-                      value={this.props.values.tags}
-                      onChange={this.props.onTagsChange}/>
 
                     <hr className="m-y-2"/>
 
